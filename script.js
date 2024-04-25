9c7ed24ee9b47d31821ce7a48ed77e2770c8cc87
Salman Sheikh <salmansheikhutk@gmail.com>
2024-04-25 14:03:57 -0400

    update

--- script.js
+++ script.js
@@ -3,11 +3,3 @@ function clickmessage() {
     console.log("Hello, WORLD!")
 }
 
-// function clickmessage() {
-//     fetch('/csvdata')
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log('Data from CSV:', data);
-//         })
-//         .catch(error => console.error('Error fetching data:', error));
-// }
