f71e379a2be9ee7e3f0b0cb541d4095c3e885d20
Salman Sheikh <salmansheikhutk@gmail.com>
2024-04-25 13:33:17 -0400

    boilerplate for html/css/js

---
+++ script.js
@@ -0,0 +1,13 @@
+function clickmessage() {
+    // alert("Button clicked")
+    console.log("Hello, WORLD!")
+}
+
+// function clickmessage() {
+//     fetch('/csvdata')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log('Data from CSV:', data);
+//         })
+//         .catch(error => console.error('Error fetching data:', error));
+// }
